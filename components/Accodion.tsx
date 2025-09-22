26796d1903bd79692aa93cdfcb3d70977ea9e30f
GozyDev <networkinggozy@gmail.com>
2025-09-22 04:09:09 +0100

    changed w of accodion

--- components/Accodion.tsx
+++ components/Accodion.tsx
@@ -68,7 +68,7 @@ export function AccordionDemo() {
   };
 
   return (
-    <section className="max-w-7xl mx-auto px-4 py-12">
+    <section className="max-w-5xl mx-auto px-4 py-12">
       <div className="space-y-4">
         {faqs.map((faq) => {
           const IconComponent =
